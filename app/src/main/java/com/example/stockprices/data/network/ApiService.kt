c47648ac401853bf37f75f227a2728142fb114a7
Valery Medvedeva <smerdovava@gmail.com>
2024-08-02 11:13:47 +0400

    added zoomChange

--- app/src/main/java/com/example/stockprices/data/network/ApiService.kt
+++ app/src/main/java/com/example/stockprices/data/network/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 interface ApiService {
 
     @GET("aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?" +
-         "adjusted=true&sort=asc&limit=50000&apiKey=NwkhSdRWbq1sRM8ofC3y8noTd5vpO4RN")
+         "adjusted=true&sort=desc&limit=50000&apiKey=NwkhSdRWbq1sRM8ofC3y8noTd5vpO4RN")
     suspend fun loadBars(): ResponseDto
 
 }
--- app/src/main/java/com/example/stockprices/presentation/MainScreen.kt
+++ app/src/main/java/com/example/stockprices/presentation/MainScreen.kt
@@ -2,26 +2,44 @@ package com.example.stockprices.presentation
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import com.example.stockprices.domain.Bar
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS_COUNT=20
 
 @Composable
 fun MainScreen(bars:List<Bar>){
+    var visibleBarCount by remember {
+        mutableStateOf(100)
+    }
+    val transformableState= TransformableState { zoomChange, _, _ ->
+        visibleBarCount=(visibleBarCount/zoomChange).roundToInt()
+            .coerceIn(MIN_VISIBLE_BARS_COUNT,bars.size)
+    }
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
+            .transformable(transformableState)
     ) {
         val maxPrice=bars.maxOf { it.highestPrice }
         val minPrice=bars.minOf { it.highestPrice }
-        val barWidth=size.width/bars.size
+        val barWidth=size.width/visibleBarCount
         val pxPerPoint = size.height/(maxPrice-minPrice)
-        bars.forEachIndexed{ index, bar->
-            val offsetX=index*barWidth
+        bars.take(visibleBarCount).forEachIndexed{ index, bar->
+            val offsetX=size.width-index*barWidth
             drawLine(
                 color = Color.White,
                 start = Offset(offsetX, size.height-(bar.lowestPrice-minPrice)*pxPerPoint),
